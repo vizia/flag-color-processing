e02b2f2ba4665894fc5a019118ead6cb1af36d21
Stefan Pearson <stefanp@brandwatch.com>
2019-02-25 13:06:28 +0000

    adds a getter fn for resolving the flag

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-module.exports = require('../public/flags');
+const flags = require('../public/flags');
+
+function getFlag(id, props = {}) {
+    const countryCode = id === 'uk' ? 'gb' : id;
+    const {viziaAssetsPath = '@vizia-assets'} = props;
+    const flag = flags.find((flag) => flag.id === countryCode);
+
+    if (!flag) {
+        return null;
+    }
+
+    return Object.assign({}, flag, {
+        src: Object.entries(flag.src)
+            .map((entry) => [entry[0], `${viziaAssetsPath}/flags/public/${entry[1]}`])
+            .reduce((memo, entry) => Object.assign(memo, {[entry[0]]: entry[1]}), {})
+    });
+}
+
+module.exports = getFlag;
