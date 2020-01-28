811eb23a94af9aa53dff457bc6899f19aab1186b
George Thomas <gthomas@brandwatch.com>
2020-01-28 15:59:32 +0000

    fix: gutil multithread

--- scripts/ci-gstorage.sh
+++ scripts/ci-gstorage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl ci-utils.bwcom.io/gcloud/install | bash
 curl ci-utils.bwcom.io/gcloud/auth | bash
-gsutil cp -r -m $TRAVIS_BUILD_DIR/public gs://cdn.vizia.brandwatch.com/assets/@vizia-assets/flags/public
-gsutil cp -r -m $TRAVIS_BUILD_DIR/src gs://cdn.vizia.brandwatch.com/assets/@vizia-assets/flags/src
+gsutil -m cp -r $TRAVIS_BUILD_DIR/public gs://cdn.vizia.brandwatch.com/assets/@vizia-assets/flags/public
+gsutil -m cp -r $TRAVIS_BUILD_DIR/src gs://cdn.vizia.brandwatch.com/assets/@vizia-assets/flags/src
