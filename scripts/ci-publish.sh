da6a4ce9bdc1b68616063cce1929c12f9542a4a7
George Thomas <gthomas@brandwatch.com>
2020-01-29 09:42:17 +0000

    chore: add gh token for login to push npm publish back

--- scripts/ci-publish.sh
+++ scripts/ci-publish.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git config --global user.email "estocker+gitbot@brandwatch.com"
 git config --global user.name "Vizia CI Bot"
+git config --global user.password ${GH_TOKEN}
 
 npx standard-version
 
