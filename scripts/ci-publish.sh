958723e5281e7fecafdaebb3d6f1ad0a690cbaff
George Thomas <gthomas@brandwatch.com>
2020-01-30 15:00:39 +0000

    chore: update flow for ci publish to get tags to master

--- scripts/ci-publish.sh
+++ scripts/ci-publish.sh
@@ -2,9 +2,11 @@
 git config --global user.email "estocker+gitbot@brandwatch.com"
 git config --global user.name "Vizia CI Bot"
 
+# ensures origin is authed
+git remote set-url origin https://${GH_TOKEN}@github.com/vizia/flags.git
+
 npx standard-version
 
 npm publish
 
-git remote add auth-remote https://${GH_TOKEN}@github.com/vizia/flags.git
-git push --follow-tags auth-remote master
+git push origin master && git push origin master --tags
